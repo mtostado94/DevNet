05274ad8e88901d7f54baf21fdb12c67338f2d01
mtostado94 <59804505+mtostado94@users.noreply.github.com>
2020-04-27 13:14:01 +0200

    Delete pruebas.py

--- EJERCICIOS/pruebas.py
+++
@@ -1,8 +0,0 @@
-from ncclient import manager
-
-con = manager.connect(host='192.168.56.101', port=830 , username='cisco', password='cisco123!', hostkey_verify=False)
-
-print("Informacion  de capabilities")
-
-for capability in con.server_capabilities:
-    print(capability)
